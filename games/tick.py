033b6beda9492166e43c7d0d7d559dd2d9d96c2d
Abelardo Gutierrez <ag2266@columbia.edu>
2020-04-25 14:01:49 -0400

    Adding comments

--- games/tick.py
+++ games/tick.py
@@ -12,6 +12,11 @@ game_count = 0
 winner = ""
 
 ########################################################################
+# Packages
+########################################################################
+from random import randint
+
+########################################################################
 # TOP-Level functions
 ########################################################################
 
@@ -59,10 +64,10 @@ def display_board():
 def make_a_move(player_token, box_selected = "none"):
     """
     Recursive function to ask users to make a move and select a numbered-box to take. This function
-    also ensures the integraty of the input by the user with the below:
-    1) If the player enter anything order than a valid number (e.g. 1-9), it will ask again for input.
-    2) If the player enters a box already taken, it will tell so to teh user and reqeust for another entry
-    After a successful move by a player, it calls check_game_status and gives turn to the game player
+    also ensures the integrity of the input by the user with the below:
+    1) If the player enters anything other than a valid number (e.g. 1-9), it will ask again for input.
+    2) If the player enters a box already taken, it will tell so to the user and request for another entry.
+    After a successful move by a player, it calls check_game_status() and gives turn to the next player
     """
     global board
     global player_setup
@@ -71,6 +76,7 @@ def make_a_move(player_token, box_selected = "none"):
     if box_selected != "none":
         print "Square {} already has a {}.".format( box_selected, board[int(box_selected)])
 
+    # The below if/else is used to keep track of the player that just made a move and assign a turn to the next player.
     if player_token == player_setup['PL1'][0]:
         player = "Player 1"
         player_setup['PL1'][1] = ""
@@ -80,6 +86,9 @@ def make_a_move(player_token, box_selected = "none"):
         player_setup['PL1'][1] = "GO"
         player_setup['PL2'][1] = ""
 
+    # In the below 8 lines we make sure a player enters a valid number for a box (1-9),checks to make sure the box is empty.
+    # If not, it will recursively ask the currrent player to make an acceptable move. Once done, it will update the board 
+    # accordinly and check if the game is over.
     move = raw_input( "{} ({}). Enter the number of the box you want to take :".format(player, player_token))
     while move not in box_number:
         move = raw_input( "{} ({}). {} is not a valid entry. Enter the number of the box you want to take :".format(player, player_token, move))
@@ -90,6 +99,12 @@ def make_a_move(player_token, box_selected = "none"):
     check_game_status()
 
 def quit_game():
+    """
+    Displays the final message once the users decide to quit the game. It will display a few stats regarding the game:
+    Games played, number of wins per user and ties.
+    """
+    global game_count
+    global standings
 
     print"\n"*100
     print "Thank you for playing !"
@@ -106,6 +121,9 @@ def quit_game():
     quit()
 
 def reset_game ():
+    """
+    Resets the game  after the players decide they want to play another game.
+    """
     global player_setup
     global board
     global winner
@@ -115,6 +133,9 @@ def reset_game ():
     winner = ""
 
 def select_player():
+    """
+    Assigns tokens to both players and designates which palyer will start first.
+    """
     global player_setup
 
     token1 = " "
@@ -131,6 +152,9 @@ def select_player():
         player_setup['PL2'] = ['X','']
 
 def welcome_display():
+    """
+    Displays the 'Welcome' message when the game first starts or after every new subsecuent  game.
+    """
     global game_count
     global standings
 
@@ -142,8 +166,8 @@ def welcome_display():
         print "#  Welcome to Tic Tac Toe Game !   #"
         print "#                                  #"
     else:
-        print "#  Welcome to Game # {}             #".format((game_count+1))
-        print "#  Standings:                      #"
+        print "#  Welcome to Game # {}             #".format((game_count + 1))
+        print "#  Standings so far:               #"
         print "#  Player 1: {} wins                #".format(standings['PL1'])
         print "#  Player 2: {} wins                #".format(standings['PL2'])
         print "#  Ties :    {}                     #".format(standings['TIE'])
@@ -163,11 +187,12 @@ def main_game_fuction ():
 
     welcome_display()
     select_player()
+    game_count +=1
     while winner == "":
         if player_setup['PL1'][1] == 'GO':
             make_a_move(player_setup['PL1'][0])
         else:
-             make_a_move(player_setup['PL2'][0])
+            make_a_move(player_setup['PL2'][0])
     display_board()
     if winner == 'TIE':
         print " WE HAVE A TIE GAME !!! "
@@ -178,7 +203,6 @@ def main_game_fuction ():
     else:
         print " PLAYER 2 WINS !!!\n"
         standings['PL2']+=1
-    game_count += 1
 
     result = raw_input('Would you like to play again: [y] for YES: ')
     if result.upper() == 'Y':
